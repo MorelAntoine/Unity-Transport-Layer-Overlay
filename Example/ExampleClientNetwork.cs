e4a699f135c374aee96f07bc3ab3ce94f0905be5
MorelAntoine <antoinemorel@outlook.com>
2019-01-28 18:09:47 -0500

    Add Example and Prefab

---
+++ Example/ExampleClientNetwork.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+namespace TransportLayerOverlay.Example
+{
+    /// <inheritdoc/>
+    /// <summary>
+    /// Example implement of ANetworkClient
+    /// </summary>
+    public sealed class ExampleClientNetwork : ANetworkClient
+    {
+        ////////////////////////////
+        ////////// Method //////////
+        ////////////////////////////
+        
+        /////////////////////////
+        ////////// API //////////
+        
+        [ContextMenu("SendHelloWorldToServer")]
+        public void SendHelloWorldToServer()
+        {
+            Send("Hello World");
+        }
+        
+        //////////////////////////////
+        ////////// Callback //////////
+        
+        public override void OnConnection(int receivedHostId, int receivedConnectionId, int receivedChannelId)
+        {
+            Debug.Log("Client connection: Host:" + receivedHostId.ToString() + " ConnectionId:" + receivedConnectionId.ToString());
+        }
+
+        public override void OnDisconnection(int receivedHostId, int receivedConnectionId, int receivedChannelId)
+        {
+            Debug.Log("Client disconnection: Host:" + receivedHostId.ToString() + " ConnectionId:" + receivedConnectionId.ToString());
+        }
+
+        public override void OnReceiveData(int receivedHostId, int receivedConnectionId, int receivedChannelId,
+            byte[] buffer, int receivedDataSize)
+        {
+            var stream = new MemoryStream(buffer);
+            var formatter = new BinaryFormatter();
+            var message = formatter.Deserialize(stream) as string;
+            
+            Debug.Log("Client received data: " + message);
+        }
+    }
+}
---
+++ Example/ExampleServerNetwork.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+namespace TransportLayerOverlay.Example
+{
+    /// <inheritdoc/>
+    /// <summary>
+    /// Basic example of an implement of the ANetworkServer
+    /// </summary>
+    public sealed class ExampleServerNetwork : ANetworkServer
+    {
+        ////////////////////////////
+        ////////// Method //////////
+        ////////////////////////////
+        
+        /////////////////////////
+        ////////// API //////////
+        
+        [ContextMenu("SendServerMasterHelloToAllClient")]
+        public void SendServerMasterHelloToAllClient()
+        {
+            Send("ServerMasterHello");
+        }
+        
+        //////////////////////////////
+        ////////// Callback //////////
+        
+        public override void OnConnection(int receivedHostId, int receivedConnectionId, int receivedChannelId)
+        {
+            base.OnConnection(receivedHostId, receivedConnectionId, receivedChannelId);
+            Debug.Log("Server connection: Host:" + receivedHostId.ToString() + " ConnectionId:" + receivedConnectionId.ToString());
+        }
+
+        public override void OnDisconnection(int receivedHostId, int receivedConnectionId, int receivedChannelId)
+        {
+            base.OnDisconnection(receivedHostId, receivedConnectionId, receivedChannelId);
+            Debug.Log("Server disconnection: Host:" + receivedHostId.ToString() + " ConnectionId:" + receivedConnectionId.ToString());
+        }
+
+        public override void OnReceiveData(int receivedHostId, int receivedConnectionId, int receivedChannelId,
+            byte[] buffer, int receivedDataSize)
+        {
+            var stream = new MemoryStream(buffer);
+            var formatter = new BinaryFormatter();
+            var message = formatter.Deserialize(stream) as string;
+            
+            Debug.Log("Server received data: " + message);
+        }
+    }
+}
